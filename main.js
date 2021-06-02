7649c0b82f161512aa09bcff4351009aa1c79a1c
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-06-02 15:16:21 +0200

    refactored

--- main.js
+++ main.js
@@ -1,32 +1,26 @@
 function convertToRoman(num) {
-  let codes = [
-    { value: 1000, char: "M" },
-    { value: 900, char: "CM" },
-    { value: 500, char: "D" },
-    { value: 400, char: "CD" },
-    { value: 100, char: "C" },
-    { value: 90, char: "XC" },
-    { value: 50, char: "L" },
-    { value: 40, char: "XL" },
-    { value: 10, char: "X" },
-    { value: 9, char: "IX" },
-    { value: 5, char: "V" },
-    { value: 4, char: "IV" },
-    { value: 1, char: "I" },
-  ];
+  let codes = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
 
   let romanNr = "";
-
   let remain = num;
-
-  for (let code of codes) {
-    let charNr = Math.floor(remain / code.value);
-
-    for (let i = 0; i < charNr; i++) {
-      romanNr += code.char;
-    }
-
-    remain -= code.value * charNr;
+  for (let code in codes) {
+    let charNr = Math.floor(remain / codes[code]);
+    romanNr += code.repeat(charNr);
+    remain -= codes[code] * charNr;
   }
 
   return romanNr;
